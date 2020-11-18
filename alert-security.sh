9a7b290595187a4ab5cbd17850c35b5b6e534781
antonionovaesjr <antonionovaesjr@gmail.com>
2020-11-18 11:53:41 -0300

    Add files via upload

---
+++ alert-security.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ cat <<-EOF > text.json
+{
+   "fields": {
+       "priority": {
+            "name": "Highest"
+        },
+       "project":
+       {
+          "key": "OS"
+       },
+       "summary": "Report de segurança do Github no ${GITHUB_REPOSITORY}",
+       "description": "Gituhub detectou vulnerabilidade no projeto",
+       "issuetype": {
+          "name": "Bug"
+        }
+    }
+}
+EOF
+
+curl -D- --user ${{ secrets.MY_JIRA_USER }}:${{ secrets.MY_JIRA_USER }} -X POST --data @text.json -H "Content-Type: application/json" ${{ secrets.MY_JIRA_URL }}
+rm -f text.json
